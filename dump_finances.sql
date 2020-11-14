6a15ecb6130a788cb5dcf7914c7dfad0876ea49c
Arnaud Clarat <46763863+ArnaudClarat@users.noreply.github.com>
2020-11-14 18:03:52 +0100

    Dump 14/11

---
+++ dump_finances.sql
@@ -0,0 +1,92 @@
+--
+-- Base de données :  `finances`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_enseignes`
+--
+
+DROP TABLE IF EXISTS `t_enseignes`;
+CREATE TABLE IF NOT EXISTS `t_enseignes` (
+  `id_enseigne` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_enseigne` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_enseigne`)
+);
+
+--
+-- Déchargement des données de la table `t_enseignes`
+--
+
+INSERT INTO `t_enseignes` (`id_enseigne`, `nom_enseigne`) VALUES
+(1, 'Colruyt');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_factures`
+--
+
+DROP TABLE IF EXISTS `t_factures`;
+CREATE TABLE IF NOT EXISTS `t_factures` (
+  `id_facture` int(11) NOT NULL AUTO_INCREMENT,
+  `magasin_facture` int(11) NOT NULL,
+  `date_facture` date NOT NULL,
+  `total_facture` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_facture`),
+  KEY `magasin_facture` (`magasin_facture`)
+);
+
+--
+-- Déchargement des données de la table `t_factures`
+--
+
+INSERT INTO `t_factures` (`id_facture`, `magasin_facture`, `date_facture`, `total_facture`) VALUES
+(1, 1, '2020-11-10', '190.00'),
+(2, 1, '2020-11-12', '9.89');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_magasins`
+--
+
+DROP TABLE IF EXISTS `t_magasins`;
+CREATE TABLE IF NOT EXISTS `t_magasins` (
+  `id_magasin` int(11) NOT NULL AUTO_INCREMENT,
+  `enseigne_magasin` int(11) NOT NULL,
+  `adresse_magasin` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_magasin`),
+  KEY `enseigne_magasin` (`enseigne_magasin`)
+);
+
+--
+-- Déchargement des données de la table `t_magasins`
+--
+
+INSERT INTO `t_magasins` (`id_magasin`, `enseigne_magasin`, `adresse_magasin`) VALUES
+(1, 1, ' Chaussée de Wavre 328, 1390 Grez-Doiceau');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_revenus`
+--
+
+DROP TABLE IF EXISTS `t_revenus`;
+CREATE TABLE IF NOT EXISTS `t_revenus` (
+  `id_revenu` int(11) NOT NULL AUTO_INCREMENT,
+  `date_revenu` date NOT NULL,
+  `montant_revenu` decimal(10,2) DEFAULT NULL,
+  `com_revenu` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_revenu`)
+);
+
+--
+-- Déchargement des données de la table `t_revenus`
+--
+
+INSERT INTO `t_revenus` (`id_revenu`, `date_revenu`, `montant_revenu`, `com_revenu`) VALUES
+(1, '2020-11-06', '315.00', 'Salaire');
+COMMIT;
